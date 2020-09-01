e34d947274a29528288651ec8a7d0061722bd846
Mr·x <as2252258@163.com>
2020-09-01 15:18:09 +0800

    改名

--- system/Processes.php
+++ system/Processes.php
@@ -44,7 +44,7 @@ class Processes extends Component
 			}
 		});
 		$server->on('message', function ($pool, $message) {
-			file_put_contents('a.log', func_get_args());
+			file_put_contents(storage('a.log'), func_get_args());
 		});
 		return $server;
 	}
