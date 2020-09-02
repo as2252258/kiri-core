046ce49386232ad11dd157aa6288bea5c0a854f3
Mr·x <as2252258@163.com>
2020-09-02 19:36:56 +0800

    改名

--- system/Abstracts/Listener.php
+++ system/Abstracts/Listener.php
@@ -8,10 +8,10 @@ namespace Snowflake\Abstracts;
  * Class Listener
  * @package Snowflake\Abstracts
  * 监听的名称
- * @property $trigger
  */
 abstract class Listener extends Component implements IListener
 {
 
+	protected $trigger = '';
 
 }
