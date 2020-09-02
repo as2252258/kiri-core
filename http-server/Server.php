cff194b3139ce23d94066ceff5d66df16cefbad9
Mr·x <as2252258@163.com>
2020-09-02 11:57:45 +0800

    改名

--- http-server/Server.php
+++ http-server/Server.php
@@ -177,16 +177,11 @@ class Server extends Application
 	{
 		if ($config['type'] == self::HTTP) {
 			$newListener->on('request', [Snowflake::createObject(OnRequest::class), 'onHandler']);
-			var_dump($config, $newListener);
 		} else if ($config['type'] == self::TCP || $config['type'] == self::PACKAGE) {
 			$newListener->on('connect', [Snowflake::createObject(OnConnect::class), 'onHandler']);
 			$newListener->on('close', [Snowflake::createObject(OnClose::class), 'onHandler']);
-			if ($config['type'] == self::PACKAGE) {
-				$newListener->on('packet', [Snowflake::createObject(OnPacket::class), 'onHandler']);
-			} else {
-				$newListener->on('receive', [Snowflake::createObject(OnReceive::class), 'onHandler']);
-			}
-			var_dump($config, $newListener);
+			$newListener->on('packet', [Snowflake::createObject(OnPacket::class), 'onHandler']);
+			$newListener->on('receive', [Snowflake::createObject(OnReceive::class), 'onHandler']);
 		} else if ($config['type'] == self::WEBSOCKET) {
 			throw new Exception('Base server must instanceof \Swoole\WebSocket\Server::class.');
 		} else {
