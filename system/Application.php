63437f77a7b2a441e855e547261011e928834f7f
Mr·x <as2252258@163.com>
2020-09-02 18:22:16 +0800

    改名

--- system/Application.php
+++ system/Application.php
@@ -50,7 +50,7 @@ class Application extends BaseApplication
 		if (!class_exists($service)) {
 			throw new NotFindClassException($service);
 		}
-		$class = Snowflake::createObject($service, [Snowflake::get()]);
+		$class = Snowflake::createObject($service);
 		if (method_exists($class, 'onImport')) {
 			$class->onImport($this);
 		}
