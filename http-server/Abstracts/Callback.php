cb89225498faf4339b458a9921ac9d198810fda1
Mr·x <as2252258@163.com>
2020-09-03 01:49:58 +0800

    改名

--- http-server/Abstracts/Callback.php
+++ http-server/Abstracts/Callback.php
@@ -94,9 +94,10 @@ abstract class Callback extends Application
 		$mail->isSMTP();                                                                  // Send using SMTP
 		$mail->Host = Config::get('email.host');                                          // Set the SMTP server to send through
 		$mail->SMTPAuth = true;                                                           // Enable SMTP authentication
+		$mail->CharSet = "GBK";                                                           // Enable SMTP authentication
 		$mail->Username = Config::get('email.username');                                  // SMTP username
 		$mail->Password = Config::get('email.password');                                  // SMTP password
-		$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;                               // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+		$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;                                  // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 		$mail->Port = Config::get('email.port');                                          // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 		$mail->setFrom(Config::get('email.send.address'), Config::get('email.send.nickname'));
 		return $mail;
