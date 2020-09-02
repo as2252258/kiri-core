d97e45e2013f9b26215295d9cd738e87531431d0
Mr·x <as2252258@163.com>
2020-09-02 18:54:25 +0800

    改名

--- http-server/Server.php
+++ http-server/Server.php
@@ -76,7 +76,7 @@ class Server extends Application
 		$annotation->register('http', Annotation::class);
 		$annotation->register('websocket', AWebsocket::class);
 
-		$this->enableCoroutine(true);
+		$this->enableCoroutine((bool)Config::get('enable_coroutine'));
 		foreach ($this->sortServers($configs) as $server) {
 			$this->create($server);
 		}
