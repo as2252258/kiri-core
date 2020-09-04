06bac597cb272e450fef0ec08efbda09716702c5
Mr·x <as2252258@163.com>
2020-09-04 11:59:39 +0800

    改名

--- HttpServer/Service/Http.php
+++ HttpServer/Service/Http.php
@@ -4,18 +4,8 @@
 namespace HttpServer\Service;
 
 
-use Exception;
-use HttpServer\Http\Context;
-use HttpServer\Http\Request as HRequest;
-use HttpServer\Http\Response as HResponse;
 use ReflectionException;
-use Snowflake\Core\JSON;
 use Snowflake\Exception\NotFindClassException;
-use Snowflake\Snowflake;
-use Swoole\Error;
-use Swoole\Http\Request;
-use Swoole\Http\Response;
-use Swoole\Http\Server;
 use HttpServer\Service\Abstracts\Http as AHttp;
 
 class Http extends AHttp
