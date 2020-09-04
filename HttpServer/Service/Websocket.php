02f38b0cfef0412ef6ba471b792c88ca82f0d5f9
Mr·x <as2252258@163.com>
2020-09-04 11:53:11 +0800

    改名

--- HttpServer/Service/Websocket.php
+++ HttpServer/Service/Websocket.php
@@ -4,18 +4,9 @@
 namespace HttpServer\Service;
 
 
-use Exception;
 use ReflectionException;
-use Snowflake\Error\Logger;
-use Snowflake\Event;
 use Snowflake\Exception\NotFindClassException;
-use Snowflake\Snowflake;
-use Swoole\Http\Request as SRequest;
-use Swoole\Http\Response as SResponse;
-use Swoole\WebSocket\Frame;
-use Swoole\WebSocket\Server;
 use HttpServer\Service\Abstracts\Websocket as HAWebsocket;
-use HttpServer\Route\Annotation\Websocket as AWebsocket;
 
 class Websocket extends HAWebsocket
 {
