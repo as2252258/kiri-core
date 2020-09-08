73399aad450451834a5eb4eed14eba3633566d78
Mr·x <as2252258@163.com>
2020-09-08 19:17:40 +0800

    改名

--- Gii/Gii.php
+++ Gii/Gii.php
@@ -41,10 +41,6 @@ class Gii
 	public $keyword = [
 		'ADD', 'ALL', 'ALTER', 'AND', 'AS', 'ASC', 'ASENSITIVE', 'BEFORE', 'BETWEEN', 'BIGINT', 'BINARY', 'BLOB', 'BOTH', 'BY', 'CALL', 'CASCADE', 'CASE', 'CHANGE', 'CHAR', 'CHARACTER', 'CHECK', 'COLLATE', 'COLUMN', 'CONDITION', 'CONNECTION', 'CONSTRAINT', 'CONTINUE', 'CONVERT', 'CREATE', 'CROSS', 'CURRENT_DATE', 'CURRENT_TIME', 'CURRENT_TIMESTAMP', 'CURRENT_USER', 'CURSOR', 'DATABASE', 'DATABASES', 'DAY_HOUR', 'DAY_MICROSECOND', 'DAY_MINUTE', 'DAY_SECOND', 'DEC', 'DECIMAL', 'DECLARE', 'DEFAULT', 'DELAYED', 'DELETE', 'DESC', 'DESCRIBE', 'DETERMINISTIC', 'DISTINCT', 'DISTINCTROW', 'DIV', 'DOUBLE', 'DROP', 'DUAL', 'EACH', 'ELSE', 'ELSEIF', 'ENCLOSED', 'ESCAPED', 'EXISTS', 'EXIT', 'EXPLAIN', 'FALSE', 'FETCH', 'FLOAT', 'FLOAT4', 'FLOAT8', 'FOR', 'FORCE', 'FOREIGN', 'FROM', 'FULLTEXT', 'GOTO', 'GRANT', 'GROUP', 'HAVING', 'HIGH_PRIORITY', 'HOUR_MICROSECOND', 'HOUR_MINUTE', 'HOUR_SECOND', 'IF', 'IGNORE', 'IN', 'INDEX', 'INFILE', 'INNER', 'INOUT', 'INSENSITIVE', 'INSERT', 'INT', 'INT1', 'INT2', 'INT3', 'INT4', 'INT8', 'INTEGER', 'INTERVAL', 'INTO', 'IS', 'ITERATE', 'JOIN', 'KEY', 'KEYS', 'KILL', 'LABEL', 'LEADING', 'LEAVE', 'LEFT', 'LIKE', 'LIMIT', 'LINEAR', 'LINES', 'LOAD', 'LOCALTIME', 'LOCALTIMESTAMP', 'LOCK', 'LONG', 'LONGBLOB', 'LONGTEXT', 'LOOP', 'LOW_PRIORITY', 'MATCH', 'MEDIUMBLOB', 'MEDIUMINT', 'MEDIUMTEXT', 'MIDDLEINT', 'MINUTE_MICROSECOND', 'MINUTE_SECOND', 'MOD', 'MODIFIES', 'NATURAL', 'NOT', 'NO_WRITE_TO_BINLOG', 'NULL', 'NUMERIC', 'ON', 'OPTIMIZE', 'OPTION', 'OPTIONALLY', 'OR', 'ORDER', 'OUT', 'OUTER', 'OUTFILE', 'PRECISION', 'PRIMARY', 'PROCEDURE', 'PURGE', 'RAID0', 'RANGE', 'READ', 'READS', 'REAL', 'REFERENCES', 'REGEXP', 'RELEASE', 'RENAME', 'REPEAT', 'REPLACE', 'REQUIRE', 'RESTRICT', 'RETURN', 'REVOKE', 'RIGHT', 'RLIKE', 'SCHEMA', 'SCHEMAS', 'SECOND_MICROSECOND', 'SELECT', 'SENSITIVE', 'SEPARATOR', 'SET', 'SHOW', 'SMALLINT', 'SPATIAL', 'SPECIFIC', 'SQL', 'SQLEXCEPTION', 'SQLSTATE', 'SQLWARNING', 'SQL_BIG_RESULT', 'SQL_CALC_FOUND_ROWS', 'SQL_SMALL_RESULT', 'SSL', 'STARTING', 'STRAIGHT_JOIN', 'TABLE', 'TERMINATED', 'THEN', 'TINYBLOB', 'TINYINT', 'TINYTEXT', 'TO', 'TRAILING', 'TRIGGER', 'TRUE', 'UNDO', 'UNION', 'UNIQUE', 'UNLOCK', 'UNSIGNED', 'UPDATE', 'USAGE', 'USE', 'USING', 'UTC_DATE', 'UTC_TIME', 'UTC_TIMESTAMP', 'VALUES', 'VARBINARY', 'VARCHAR', 'VARCHARACTER', 'VARYING', 'WHEN', 'WHERE', 'WHILE', 'WITH', 'WRITE', 'X509', 'XOR', 'YEAR_MONTH', 'ZEROFILL'
 	];
-	/**
-	 * @var bool
-	 */
-	private $isUpdate = false;
 
 	/**
 	 * @param Connection|null $db
@@ -59,6 +55,30 @@ class Gii
 	{
 		$this->input = $input;
 		if (!empty($db)) $this->db = $db;
+
+		$make = $this->input->get('make', null);
+		if (empty($make)) {
+			throw new Exception('构建类型不能为空~');
+		}
+		switch ($make) {
+			case 'task':
+				$task = new GiiTask();
+				return $task->generate();
+			default:
+				return $this->getModel($make, $input);
+		}
+	}
+
+
+	/**
+	 * @param $make
+	 * @param $input
+	 * @return array
+	 * @throws ComponentException
+	 * @throws ConfigException
+	 */
+	private function getModel($make, $input)
+	{
 		if (!$this->db) {
 			$db = $this->input->get('databases', 'db');
 			$this->db = Snowflake::app()->db->get($db);
@@ -68,17 +88,16 @@ class Gii
 			$this->tableName = $input->get('table');
 			$redis->del('column:' . $this->tableName);
 		}
-		if ($input->get('model', NULL)) {
-			$model = 1;
+		switch ($make) {
+			case 'controller':
+				return $this->getTable(1, 0);
+			case 'model':
+				return $this->getTable(0, 1);
+			default:
+				return [];
 		}
-		if ($input->get('controller', NULL)) {
-			$controller = 1;
-		}
-		if ($input->get('isUpdate') == 1) {
-			$this->isUpdate = TRUE;
-		}
-		return $this->getTable($controller, $model);
 	}
+
 
 	/**
 	 * @param $controller
@@ -87,7 +106,7 @@ class Gii
 	 *
 	 * @throws Exception
 	 */
-	private function getTable(&$controller, &$model)
+	private function getTable($controller, $model)
 	{
 		$tables = $this->getFields($this->getTables());
 		if (empty($tables)) {
---
+++ Gii/GiiTask.php
@@ -0,0 +1,100 @@
+<?php
+
+
+namespace Gii;
+
+use App\Async\UserOnline;
+use Database\Db;
+use Exception;
+use HttpServer\IInterface\Task;
+use Snowflake\Snowflake;
+
+/**
+ * Class GiiModel
+ * @package Gii
+ */
+class GiiTask extends GiiBase
+{
+
+	public $classFileName;
+	public $tableName;
+	public $visible;
+	public $res;
+	public $fields;
+
+
+	/**
+	 * @return string[]
+	 * @throws Exception
+	 */
+	public function generate()
+	{
+
+		$managerName = $this->input->get('name', null);
+		if (empty($managerName)) {
+			throw new Exception('文件名称不能为空~');
+		}
+		$html = '<?php
+		
+		
+namespace App\Async;
+
+use HttpServer\IInterface\Task;
+
+';
+
+		$html .= '
+
+/**
+ * Class ' . ucfirst($managerName) . '
+ * @package Inter\mysql
+ *' . implode('', $this->visible) . '
+ * @sql
+ */
+class ' . ucfirst($managerName) . ' implements Task
+{
+	
+	protected $params = [];
+
+
+	/**
+	 * @return mixed|void
+	 */
+	public function onHandler()
+	{
+		// TODO: Implement handler() method.
+	}
+
+
+	/**
+	 * @param $params
+	 * @return $this
+	 */
+	public function setParams(array $params)
+	{
+		$this->params = $params;
+		return $this;
+	}
+
+
+	/**
+	 * @return array
+	 */
+	public function getParams()
+	{
+		return $this->params;
+	}
+
+
+}';
+
+		$file = APP_PATH . '/app/Async/' . $managerName . '.php';
+		if (file_exists($file)) {
+			unlink($file);
+		}
+
+		Snowflake::writeFile($file, $html);
+		return [$managerName . '.php'];
+	}
+
+}
