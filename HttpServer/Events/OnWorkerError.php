e58e388d3b8a4d0b0d3195cbb63c62847b04285d
Mr·x <as2252258@163.com>
2020-09-09 18:50:46 +0800

    改名

--- HttpServer/Events/OnWorkerError.php
+++ HttpServer/Events/OnWorkerError.php
@@ -28,12 +28,12 @@ class OnWorkerError extends Callback
 		if (!Config::has('email')) {
 			return;
 		}
-//		Coroutine::create([$this,'system_mail'],print_r([
-//			'$worker_pid' => $worker_pid,
-//			'$worker_id'  => $worker_id,
-//			'$exit_code'  => $exit_code,
-//			'$signal'     => $signal,
-//		], true));
+		$this->system_mail(print_r([
+			'$worker_pid' => $worker_pid,
+			'$worker_id'  => $worker_id,
+			'$exit_code'  => $exit_code,
+			'$signal'     => $signal,
+		], true));
 	}
 
 }
