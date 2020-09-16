cad74ecdce1196e4c2ddcc70702aa82446de952a
Mr·x <as2252258@163.com>
2020-09-16 11:09:43 +0800

    改名

--- function.php
+++ function.php
@@ -59,6 +59,7 @@ if (!function_exists('loadByDir')) {
 				$first = explode(DIRECTORY_SEPARATOR, $replace);
 				array_shift($first);
 
+				var_dump($namespace . '\\' . implode('\\', $first));
 				Snowflake::setAutoload($namespace . '\\' . implode('\\', $first), $value);
 			}
 		}
