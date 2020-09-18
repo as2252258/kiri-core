8cf663fcda6106108b718a11cecbe2e7286e78b1
Mr·x <as2252258@163.com>
2020-09-18 22:02:29 +0800

    改名

--- HttpServer/Route/Annotation/Websocket.php
+++ HttpServer/Route/Annotation/Websocket.php
@@ -48,18 +48,6 @@ class Websocket extends Annotation
 
 
 	/**
-	 * @param string $path
-	 * @param string $namespace
-	 * @throws \ReflectionException
-	 */
-	public function registration_notes($path = '', $namespace = '')
-	{
-		var_dump(get_called_class());
-		parent::registration_notes($path, $namespace);
-	}
-
-
-	/**
 	 * @param $events
 	 * @param $comment
 	 * @return false|string
