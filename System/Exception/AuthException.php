857d4490eb621fe638451515d6f2fe10fa93b36f
Mr·x <as2252258@163.com>
2020-09-21 13:46:28 +0800

    改名

--- System/Exception/AuthException.php
+++ System/Exception/AuthException.php
@@ -4,7 +4,14 @@
 namespace Snowflake\Exception;
 
 
+use Throwable;
+
 class AuthException extends \Exception
 {
+
+	public function __construct($message = "", $code = 0, Throwable $previous = null)
+	{
+		parent::__construct($message, 401, $previous);
+	}
 
 }
