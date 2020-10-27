0ea78d03a8533cfe4a64266a98033b1e19734e98
Mr·x <as2252258@163.com>
2020-10-27 19:37:26 +0800

    改名

--- Kafka/Kafka.php
+++ Kafka/Kafka.php
@@ -145,7 +145,7 @@ class Kafka extends \Snowflake\Process\Process
 			}
 		});
 		$conf->set('group.id', uniqid('kafka'));
-		$conf->set('metadata.broker.list', '127.0.0.1:9092');
+		$conf->set('metadata.broker.list', '127.0.0.1:2080');
 
 		$topicConf = new TopicConf();
 		$topicConf->set('auto.commit.enable', 1);
