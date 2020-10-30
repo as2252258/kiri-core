d5920f3972822b2924bab5dbaf1177554484773d
Mr·x <as2252258@163.com>
2020-10-30 11:24:00 +0800

    改名

--- Kafka/Producer.php
+++ Kafka/Producer.php
@@ -62,7 +62,7 @@ class Producer extends Component
 	 */
 	public function setAck(bool $value)
 	{
-		$this->topicConf->set('request.required.acks', (string)$value);
+//		$this->topicConf->set('request.required.acks', (string)$value);
 		return $this;
 	}
 
