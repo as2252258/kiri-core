dc596b642c85f33c760f12431e66d32df93d133c
Mr·x <as2252258@163.com>
2020-12-15 18:52:50 +0800

    改名

--- Annotation/Route/Node.php
+++ Annotation/Route/Node.php
@@ -3,6 +3,10 @@
 
 namespace Annotation\Route;
 
+use HttpServer\IInterface\After;
+use HttpServer\IInterface\Interceptor;
+use HttpServer\IInterface\Limits;
+use HttpServer\IInterface\Middleware;
 use HttpServer\Route\Node as RNode;
 use ReflectionException;
 use Snowflake\Exception\NotFindClassException;
@@ -45,7 +49,19 @@ trait Node
 	{
 		$di = Snowflake::getDi();
 		foreach ($classes as $key => $class) {
-			$classes[$key] = [$di->get($class), 'onHandler'];
+			$object = $di->get($class);
+			if ($object instanceof Interceptor) {
+				$classes[$key] = [$object, 'Interceptor'];
+			}
+			if ($object instanceof Limits) {
+				$classes[$key] = [$object, 'next'];
+			}
+			if ($object instanceof After) {
+				$classes[$key] = [$object, 'onHandler'];
+			}
+			if ($object instanceof Middleware) {
+				$classes[$key] = [$object, 'onHandler'];
+			}
 		}
 		return $classes;
 	}
