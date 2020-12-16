cc219221e7905216e432bbd3f699ba4fd7cfe787
Mr·x <as2252258@163.com>
2020-12-16 16:39:50 +0800

    改名

--- HttpServer/Route/Middleware.php
+++ HttpServer/Route/Middleware.php
@@ -92,18 +92,20 @@ class Middleware
 			return;
 		}
 		foreach ($annotation as $item) {
-			var_dump($item);
-			if ($item instanceof Interceptor) {
-				$node->addInterceptor($item->interceptor);
-			}
-			if ($item instanceof After) {
-				$node->addAfter($item->after);
-			}
-			if ($item instanceof RMiddleware) {
-				$node->addMiddleware($item->middleware);
-			}
-			if ($item instanceof Limits) {
-				$node->addLimits($item->limits);
+			foreach ($item['attributes'] as $attribute) {
+				var_dump($attribute);
+				if ($attribute instanceof Interceptor) {
+					$node->addInterceptor($attribute->interceptor);
+				}
+				if ($attribute instanceof After) {
+					$node->addAfter($attribute->after);
+				}
+				if ($attribute instanceof RMiddleware) {
+					$node->addMiddleware($attribute->middleware);
+				}
+				if ($attribute instanceof Limits) {
+					$node->addLimits($attribute->limits);
+				}
 			}
 		}
 	}
