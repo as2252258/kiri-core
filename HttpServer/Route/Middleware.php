274748ae1937546af0446efc32e4505156a9d326
Mr·x <as2252258@163.com>
2020-12-16 16:53:18 +0800

    改名

--- HttpServer/Route/Middleware.php
+++ HttpServer/Route/Middleware.php
@@ -86,7 +86,6 @@ class Middleware
 					$node->addAfter($attribute->after);
 				}
 				if ($attribute instanceof RMiddleware) {
-					var_dump($attribute->middleware);
 					$node->addMiddleware($attribute->middleware);
 				}
 				if ($attribute instanceof Limits) {
@@ -108,7 +107,7 @@ class Middleware
 		foreach ($node->getMiddleWares() as $middleWare) {
 			$middleWares[] = $middleWare;
 		}
-		$this->middleWares = [];
+		var_dump($middleWares);
 		return $middleWares;
 	}
 
