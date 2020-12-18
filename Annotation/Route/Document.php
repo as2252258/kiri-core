bc05d3de16953a0d1ca1861fbef578aca940c213
Mr·x <as2252258@163.com>
2020-12-18 11:55:16 +0800

    改名

--- Annotation/Route/Document.php
+++ Annotation/Route/Document.php
@@ -11,6 +11,19 @@ namespace Annotation\Route;
 #[\Attribute(\Attribute::TARGET_METHOD)] class Document
 {
 
+	const INTEGER = 'int';
+	const STRING = 'string';
+	const BOOLEAN = 'bool';
+	const FLOAT = 'float';
+
+	const ALIAS = [
+		self::INTEGER => '整数',
+		self::STRING  => '字符串',
+		self::BOOLEAN => '布尔值',
+		self::FLOAT   => '浮点',
+	];
+
+
 	public function __construct(
 		public array $docs
 	)
