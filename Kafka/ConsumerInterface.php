41dbb91936fa630a3d48fa901a4d443cce36314a
Mr·x <as2252258@163.com>
2020-12-23 16:28:40 +0800

    改名

--- Kafka/ConsumerInterface.php
+++ Kafka/ConsumerInterface.php
@@ -16,7 +16,7 @@ interface ConsumerInterface
 	 * @param Struct $struct
 	 * @return mixed
 	 */
-	public function onHandler(Struct $struct): mixed;
+	public function onHandler(Struct $struct): void;
 
 
 }
