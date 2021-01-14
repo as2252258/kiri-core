e8086cf6d0b15397e349e13802014f976b39319b
Mr·x <as2252258@163.com>
2021-01-14 10:35:44 +0800

    改名

--- Kafka/Kafka.php
+++ Kafka/Kafka.php
@@ -159,13 +159,14 @@ class Kafka extends \Snowflake\Process\Process
 		try {
 			$topicConf->set('auto.commit.enable', '1');
 			$topicConf->set('auto.commit.interval.ms', '100');
-			$topicConf->set('auto.create.topics.enable', 'true');
 
 			//smallest：简单理解为从头开始消费，
 			//largest：简单理解为从最新的开始消费
 			$topicConf->set('auto.offset.reset', 'smallest');
 			$topicConf->set('offset.store.path', 'kafka_offset.log');
 			$topicConf->set('offset.store.method', 'broker');
+
+			$conf->set('auto.create.topics.enable', true);
 		} catch (Throwable $exception) {
 			var_dump($exception->getMessage());
 		}
