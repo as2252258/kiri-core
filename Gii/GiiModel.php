1ee6a05860cd1fcba9021125e6836cc11bf05d27
Mr·x <as2252258@163.com>
2021-01-19 19:56:08 +0800

    改名

--- Gii/GiiModel.php
+++ Gii/GiiModel.php
@@ -126,9 +126,6 @@ class ' . $managerName . ' extends ActiveRecord
 					$html .= '
     ' . $debug . ' static $' . $key . ' = ' . $val . ';' . "\n";
 				} else {
-					if ($key == 'primary') {
-						continue;
-					}
 					$html .= '
     ' . $debug . ' $' . $key . ' = ' . $val . ';' . "\n";
 				}
