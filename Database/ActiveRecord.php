d639504842c2855f2e11470b58ec0b6f3bc1da49
Mr·x <as2252258@163.com>
2021-01-19 19:14:18 +0800

    改名

--- Database/ActiveRecord.php
+++ Database/ActiveRecord.php
@@ -313,6 +313,8 @@ class ActiveRecord extends BaseActiveRecord
 		$attributes = Snowflake::app()->getAttributes();
 		$callback = $attributes->getByClass(static::class);
 
+		var_dump($callback);
+
 		$data = $this->_attributes;
 		foreach ($callback as $key => $item) {
 			$data[$key] = call_user_func($item, $data[$key]);
