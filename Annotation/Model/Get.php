f29d96ffc06a8409ed2e1f0b70ec0384377dbb01
Mr·x <as2252258@163.com>
2021-01-19 17:45:33 +0800

    改名

---
+++ Annotation/Model/Get.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace Annotation\Model;
+
+
+use Attribute;
+
+
+/**
+ * Class Get
+ * @package Annotation\Model
+ */
+#[Attribute(Attribute::TARGET_METHOD)] class Get
+{
+
+
+	public function __construct()
+	{
+
+	}
+
+
+}
