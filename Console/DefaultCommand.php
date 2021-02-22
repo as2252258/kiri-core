310978b6e9c5683886c05b7223837edb84281cda
Mr·x <as2252258@163.com>
2021-02-22 11:42:52 +0800

    改名

--- Console/DefaultCommand.php
+++ Console/DefaultCommand.php
@@ -29,17 +29,17 @@ class DefaultCommand extends Command
 		foreach ($param as $key => $val) {
 			$split = explode(':', $key);
 			if (empty($last) && isset($split[0])) {
-				$lists[] = "\033[32;40;1;1m" . $split[0] . " \033[0m\t";
+				$lists[] = "\033[32;40;1;1m" . $split[0] . " \033[0m\v";
 			} else if (isset($split[0]) && $last != $split[0]) {
-				$lists[] = "\033[32;40;1;1m" . $split[0] . " \033[0m\t";
+				$lists[] = "\033[32;40;1;1m" . $split[0] . " \033[0m\v";
 			}
 
 			$last = $split[0] ?? '';
 
 			list($method, $ts) = $val;
-			$lists[] = "\033[32;40;1;1m  " . $key . " \033[0m\t" . $method;
+			$lists[] = "\033[32;40;1;1m  " . $key . " \033[0m\v" . $method;
 		}
-		return implode("\v", $lists);
+		return implode("\t", $lists);
 	}
 
 }
