181e3c0c3bd136150ac67f05608b1d668ed5a7da
Mr·x <as2252258@163.com>
2021-02-22 11:52:11 +0800

    改名

--- Console/DefaultCommand.php
+++ Console/DefaultCommand.php
@@ -25,19 +25,19 @@ class DefaultCommand extends Command
 		$param = $dtl->get('commandList');
 
 		$last = '';
-		$lists = ["\vCommands\t" . '注释'];
+		$lists = ["Commands\t" . '注释'];
 		foreach ($param as $key => $val) {
 			$split = explode(':', $key);
 			if (empty($last) && isset($split[0])) {
-				$lists[] = "\v\033[32;40;1;1m" . $split[0] . " \033[0m\t";
+				$lists[] = "\033[32;40;1;1m" . $split[0] . " \033[0m\t";
 			} else if (isset($split[0]) && $last != $split[0]) {
-				$lists[] = "\v\033[32;40;1;1m" . $split[0] . " \033[0m\t";
+				$lists[] = "\033[32;40;1;1m" . $split[0] . " \033[0m\t";
 			}
 
 			$last = $split[0] ?? '';
 
 			list($method, $ts) = $val;
-			$lists[] = "\v\033[32;40;1;1m  " . $key . " \033[0m\t" . $method;
+			$lists[] = "\033[32;40;1;1m  " . $key . " \033[0m\t\v" . $method."\v";
 		}
 		return implode(PHP_EOL, $lists);
 	}
