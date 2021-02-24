fe36053ce023fb499eadac2c1f24ee8af6357269
Mr·x <as2252258@163.com>
2021-02-24 18:56:58 +0800

    改名

--- Database/Orm/Condition.php
+++ Database/Orm/Condition.php
@@ -78,6 +78,7 @@ trait Condition
 		$_tmp = [];
 		if (empty($where)) return '';
 		foreach ($where as $key => $value) {
+			var_dump($value);
 			$_value = is_string($value) ? $value : $this->conditionMap($value);
 
 			if (empty($_value)) continue;
