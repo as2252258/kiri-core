f5ce3ab2a7c287366b3114061364acdbfa9100e6
Mr·x <as2252258@163.com>
2021-02-25 18:11:35 +0800

    改名

--- Database/Base/BaseActiveRecord.php
+++ Database/Base/BaseActiveRecord.php
@@ -585,6 +585,9 @@ abstract class BaseActiveRecord extends Component implements IOrm, ArrayAccess
 				$condition[$key] = $val;
 			}
 		}
+
+		var_dump($this->_oldAttributes, $this->_attributes);
+
 		return [$_tmp, $condition, array_keys($_tmp)];
 	}
 
