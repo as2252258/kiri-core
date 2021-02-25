e03079866b60ddb5b45cf805e4ecf24303973176
Mr·x <as2252258@163.com>
2021-02-25 16:46:42 +0800

    改名

--- Database/SqlBuilder.php
+++ Database/SqlBuilder.php
@@ -44,9 +44,7 @@ class SqlBuilder extends Component
 			return $this->addError('None data update.');
 		}
 
-		var_dump($string);
-
-		$update = 'UPDATE ' . $this->tableName() . ' SET ' . $string . $this->conditionToString();
+		$update = 'UPDATE ' . $this->tableName() . ' SET ' . implode(',', $string) . $this->conditionToString();
 		$update .= $this->builderLimit($this->query);
 
 		return [$update, $array];
