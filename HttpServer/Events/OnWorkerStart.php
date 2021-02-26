f5d09ee4e2c02943336aa372e3370f56d64542d4
Mr·x <as2252258@163.com>
2021-02-26 20:01:33 +0800

    改名

--- HttpServer/Events/OnWorkerStart.php
+++ HttpServer/Events/OnWorkerStart.php
@@ -64,8 +64,8 @@ class OnWorkerStart extends Callback
 		if ($sigkill === false) {
 			return $server->stop($workerId);
 		}
-		while ($server->stats()['coroutine_num'] > 1) {
-			Coroutine::sleep(0.01);
+		while ($server->stats()['coroutine_num'] > 0) {
+			sleep(0.01);
 		}
 		return $server->stop($workerId);
 	}
