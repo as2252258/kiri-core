ddddb17c7a8c64406cbcf01a75b8cfa78b23b8c3
as2252258@163.com <as22522582>
2021-02-28 00:28:54 +0800

    modify

--- HttpServer/Events/OnManagerStart.php
+++ HttpServer/Events/OnManagerStart.php
@@ -9,26 +9,28 @@ use HttpServer\Abstracts\Callback;
 use Snowflake\Abstracts\Config;
 use Snowflake\Event;
 use Snowflake\Snowflake;
+use Swoole\Coroutine;
 use Swoole\Server;
 
 class OnManagerStart extends Callback
 {
 
-	/**
-	 * @param Server $server
-	 * @throws Exception
-	 */
-	public function onHandler(Server $server)
-	{
-		$this->debug('manager start.');
-		Snowflake::setWorkerId($server->manager_pid);
+    /**
+     * @param Server $server
+     * @throws Exception
+     */
+    public function onHandler(Server $server)
+    {
+        $this->debug('manager start.');
+        Snowflake::setWorkerId($server->manager_pid);
 
-		$events = Snowflake::app()->getEvent();
-		$events->trigger(Event::SERVER_MANAGER_START, null, $server);
-		if (Snowflake::isLinux()) {
-			$prefix = Config::get('id', false, 'system:');
-			name($prefix . ': Server Manager.');
-		}
-	}
+        $events = Snowflake::app()->getEvent();
+        $events->trigger(Event::SERVER_MANAGER_START, null, $server);
+        if (Snowflake::isLinux()) {
+            $prefix = Config::get('id', false, 'system:');
+            name($prefix . ': Server Manager.');
+        }
+        Coroutine::waitPid($server->manager_pid);
+    }
 
 }
