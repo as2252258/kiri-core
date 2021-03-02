1d2ee758fb84c6a3b100dc591db5d8aac217e53f
Mr·x <as2252258@163.com>
2021-03-02 17:23:59 +0800

    改名

--- System/Abstracts/IListener.php
+++ System/Abstracts/IListener.php
@@ -1,16 +1,26 @@
 <?php
 
 declare(strict_types=1);
+
 namespace Snowflake\Abstracts;
 
 
 use Snowflake\Core\Dtl;
 
+
+/**
+ * Interface IListener
+ * @package Snowflake\Abstracts
+ */
 interface IListener
 {
 
 
-	public function handler(Dtl $dtl);
+	/**
+	 * @param Dtl $dtl
+	 * @return mixed
+	 */
+	public function execute(Dtl $dtl): mixed;
 
 
 }
