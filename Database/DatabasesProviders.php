27e928b897242eb09d644aa3ea6a1e92a8ddd729
Mr·x <as2252258@163.com>
2021-03-02 17:53:27 +0800

    改名

--- Database/DatabasesProviders.php
+++ Database/DatabasesProviders.php
@@ -38,7 +38,7 @@ class DatabasesProviders extends Providers
 
 
 		$event->on(Event::SERVER_WORKER_START, [$this, 'scanModel']);
-//		$event->on(Event::SERVER_TASK_START, [$this, 'scanModel']);
+		$event->on(Event::SERVER_TASK_START, [$this, 'scanModel']);
 	}
 
 
