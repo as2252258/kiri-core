5aa1a562b0c750fba55fc2e57cedc01220d8f6c2
Mr·x <as2252258@163.com>
2021-03-03 13:46:42 +0800

    改名

--- HttpServer/Events/OnHandshake.php
+++ HttpServer/Events/OnHandshake.php
@@ -112,9 +112,8 @@ class OnHandshake extends Callback
 			$sRequest = Request::create($request);
 			$sRequest->uri = '/' . Socket::HANDSHAKE . '::event';
 			$sRequest->headers->replace('request_method', 'sw::socket');
+			$sRequest->headers->replace('request_uri', $sRequest->uri);
 			$sRequest->parseUri();;
-
-			var_dump($sRequest);
 
 			if (($node = $router->find_path($sRequest)) === null) {
 				return $this->disconnect($response, 502);
