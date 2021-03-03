d6c9291b1a41700c5366b3f7e2140340a418faca
Mr·x <as2252258@163.com>
2021-03-04 00:21:18 +0800

    改名

--- HttpServer/Route/CoreMiddleware.php
+++ HttpServer/Route/CoreMiddleware.php
@@ -29,6 +29,8 @@ class CoreMiddleware implements \HttpServer\IInterface\Middleware
 	{
 		$headers = $request->headers;
 
+		var_dump(get_called_class());
+
 		/** @var Response $response */
 		$response = Context::getContext('response');
 		$response->addHeader('Access-Control-Allow-Origin', '*');
