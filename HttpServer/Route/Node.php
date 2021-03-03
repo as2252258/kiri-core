862eb0fd4baa7f14828372481a1faf61437f4f66
Mr·x <as2252258@163.com>
2021-03-04 00:36:26 +0800

    改名

--- HttpServer/Route/Node.php
+++ HttpServer/Route/Node.php
@@ -406,6 +406,7 @@ class Node extends HttpService
 			$class = [$class];
 		}
 		foreach ($class as $closure) {
+			var_export($closure);
 			if (is_string($closure)) {
 				$closure = [Snowflake::createObject($closure), 'onHandler'];
 			}
