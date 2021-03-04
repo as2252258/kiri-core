a82bd43c25a9798a86bdefd788e189e2e30ff9c7
Mr·x <as2252258@163.com>
2021-03-04 15:18:37 +0800

    改名

--- Database/ActiveRecord.php
+++ Database/ActiveRecord.php
@@ -300,6 +300,9 @@ class ActiveRecord extends BaseActiveRecord
 			return $relates;
 		}
 		foreach ($this->_with as $val) {
+
+			var_dump($val);
+
 			$relates[$val] = $this->resolveObject($val);
 		}
 		return $relates;
