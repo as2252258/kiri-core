0da515b3a7b9899a4a4bd20354e799d61429c57a
Mr·x <as2252258@163.com>
2021-03-05 17:13:23 +0800

    改名

--- Annotation/Route/Route.php
+++ Annotation/Route/Route.php
@@ -46,6 +46,10 @@ use Snowflake\Snowflake;
 		$node = $router->addRoute($this->uri, $handler, $this->method);
 		$node::annotationInject($node, get_class($handler[0]), $handler[1]);
 
+		if ($this->uri == 'switch/map') {
+			var_dump($node);
+		}
+
 		return $router;
 	}
 
