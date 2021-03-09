2f61803469b52e87f56320ffd298f80695aefb9e
Mr·x <as2252258@163.com>
2021-03-09 19:16:55 +0800

    改名

--- HttpServer/Http/Response.php
+++ HttpServer/Http/Response.php
@@ -300,9 +300,9 @@ class Response extends HttpService
 		$this->response->setHeader('cache-control', 'max-age=600');
 		$this->response->setHeader('vary', 'Accept-Encoding');
 		$this->response->setHeader('via', '1.1 varnish');
-		$this->response->setHeader('content-encoding', 'gzip');
+//		$this->response->setHeader('content-encoding', 'gzip');
 //		$this->response->setHeader('Content-Disposition', ' attachment; filename="' . end($name) . '"');
-//		$this->response->gzip(5);
+		$this->response->gzip(5);
 
 		$this->response->sendfile($path);
 //
