ce8f8795e3a4ed6e81f0d990ad53ed00525dfbde
Mr·x <as2252258@163.com>
2021-03-19 17:09:52 +0800

    改名

--- System/Async.php
+++ System/Async.php
@@ -6,6 +6,7 @@ namespace Snowflake;
 
 use Exception;
 use HttpServer\IInterface\Task;
+use ReflectionException;
 use Snowflake\Abstracts\Component;
 
 /**
@@ -17,15 +18,21 @@ class Async extends Component
 
 
 	/**
-	 * @param Task $class
+	 * @param string $class
+	 * @param array $params
 	 * @throws Exception
+	 * @throws ReflectionException
 	 */
-	public function dispatch(Task $class)
+	public function dispatch(string $class, array $params)
 	{
 		$server = Snowflake::app()->getSwoole();
 		if (!isset($server->setting['task_worker_num']) || !class_exists($class)) {
 			return;
 		}
+
+		/** @var Task $class */
+		$class = Snowflake::createObject($class);
+		$class->setParams($params);
 
 		$randWorkerId = random_int(0, $server->setting['task_worker_num'] - 1);
 
