b98bf15998c39548add6eb0077b1ba3f530a40d4
Mr·x <as2252258@163.com>
2021-03-29 16:21:52 +0800

    改名

--- Annotation/Annotation.php
+++ Annotation/Annotation.php
@@ -43,6 +43,7 @@ class Annotation extends Component
 	 */
 	public function injectProperty(object $class)
 	{
+		var_dump(__METHOD__);
 		$this->_loader->injectProperty(get_class($class), $class);
 	}
 
