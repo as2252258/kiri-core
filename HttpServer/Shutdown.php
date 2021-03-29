baa05a2173b06adccdce1725769027ebd05aaa31
Mr·x <as2252258@163.com>
2021-03-29 10:45:41 +0800

    改名

--- HttpServer/Shutdown.php
+++ HttpServer/Shutdown.php
@@ -93,7 +93,7 @@ class Shutdown extends Component
 		}
 		$shell = 'ps -eo pid,cmd,state | grep %d | grep -v grep';
 		exec(sprintf($shell, $content), $output, $code);
-		var_dump($output, $code);
+		var_dump($content, $output, $code);
 		if (empty($output)) {
 			return false;
 		}
