e41769f4a9d1612d34e572cfccc93b72286722d2
Mr·x <as2252258@163.com>
2021-03-29 17:13:16 +0800

    改名

--- System/Abstracts/BaseObject.php
+++ System/Abstracts/BaseObject.php
@@ -36,10 +36,10 @@ class BaseObject implements Configure
 		if (!empty($config) && is_array($config)) {
 			Snowflake::configure($this, $config);
 		}
-		$app = Snowflake::app();
-		if ($app instanceof Application && $app->has('attributes')) {
-			annotation()->injectProperty($this);
-		}
+//		$app = Snowflake::app();
+//		if ($app instanceof Application && $app->has('attributes')) {
+//			annotation()->injectProperty($this);
+//		}
 		$this->initialization();
 	}
 
