7d9d623297adac9b0d71a9779031163d59fd25c9
Mr·x <as2252258@163.com>
2021-03-30 19:31:12 +0800

    改名

--- System/Core/Number.php
+++ System/Core/Number.php
@@ -8,15 +8,24 @@ class Number
 {
 
 
-    /**
-     * @param int $userId
-     * @return string
-     */
-    public static function order(int $userId): string
-    {
-        $explode = current(explode(' ', str_replace('0.', '', microtime())));
+	/**
+	 * @param int $userId
+	 * @return string
+	 */
+	public static function order(int $userId): string
+	{
+		$explode = current(explode(' ', str_replace('0.', '', microtime())));
 
-        return 'No.' . sprintf('%09d', $userId) . '.' . date('Ymd.His') . '.' . $explode;
-    }
+		return 'No.' . sprintf('%09d', $userId) . '.' . date('Ymd.His') . '.' . $explode;
+	}
+
+	/**
+	 * @param int $userId
+	 * @return string
+	 */
+	public static function create(int $userId): string
+	{
+		return static::order($userId);
+	}
 
 }
