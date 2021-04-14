03f365b1f5e7c2addd35ad5d2567918745196d38
Mr·x <as2252258@163.com>
2021-04-14 18:29:07 +0800

    改名

--- Annotation/Loader.php
+++ Annotation/Loader.php
@@ -325,8 +325,8 @@ class Loader extends BaseObject
 	{
 		foreach ($nodes as $node) {
 			$this->execute($node->getFiles());
-			$this->addError($node->getDirPath() . '-->' . $outPath);
 			if (!empty($outPaht) && str_contains($node->getDirPath(), $outPath)) {
+				$this->addError($node->getDirPath() . '-->' . $outPath);
 				continue;
 			}
 			$childes = $node->getChildes();
