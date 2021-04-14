a3c4f2858b965d5a513899b32b385b97b395c78b
Mr·x <as2252258@163.com>
2021-04-14 17:16:22 +0800

    改名

--- HttpServer/Events/OnHandshake.php
+++ HttpServer/Events/OnHandshake.php
@@ -136,6 +136,8 @@ class OnHandshake extends Callback
 
 		$sRequest->parseUri();
 
+		var_dump($sRequest);
+
 		if (($node = $router->find_path($sRequest)) === null) {
 			return $this->disconnect($response, 502);
 		}
