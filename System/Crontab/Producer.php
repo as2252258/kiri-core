2f5f86a5ab7a8b2a1334f57866f9e9388f4b0728
Mr·x <as2252258@163.com>
2021-04-15 16:25:49 +0800

    改名

--- System/Crontab/Producer.php
+++ System/Crontab/Producer.php
@@ -39,6 +39,9 @@ class Producer extends Component
 
 		$result = $redis->zAdd(self::CRONTAB_KEY, $tickTime, $name);
 		if ($result) {
+
+			var_dump($crontab);
+
 			$redis->set('crontab:' . $name, swoole_serialize($crontab));
 		}
 	}
