ee8cb7d06be661de103e4ff83a0627dc970763bc
Mr·x <as2252258@163.com>
2021-04-19 15:18:41 +0800

    改名

--- HttpServer/Events/OnWorkerStart.php
+++ HttpServer/Events/OnWorkerStart.php
@@ -90,9 +90,8 @@ class OnWorkerStart extends Callback
 			name($server->worker_pid, 'Worker#' . $server->worker_id);
 
 			$time = microtime(true);
-			$annotation->runtime(CONTROLLER_PATH);
+			$annotation->runtime(directory('app'));
 			$this->debug('use time.' . microtime(true) - $time);
-			$annotation->runtime(directory('app'), CONTROLLER_PATH);
 
 			Snowflake::setWorkerId($server->worker_pid);
 			putenv('environmental=' . Snowflake::WORKER);
