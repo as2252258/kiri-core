6ab9b2401866f5951aaf74f7051e5f4e3b7f2587
Mr·x <as2252258@163.com>
2021-04-19 15:26:34 +0800

    改名

--- HttpServer/Events/OnWorkerStart.php
+++ HttpServer/Events/OnWorkerStart.php
@@ -65,12 +65,7 @@ class OnWorkerStart extends Callback
 	{
 		putenv('environmental=' . Snowflake::TASK);
 
-		$annotation->runtime(directory('app'), [
-			CONTROLLER_PATH,
-			LISTENER_PATH,
-			SOCKET_PATH,
-			TASK_PATH,
-		]);
+		$annotation->runtime(MODEL_PATH);
 		name($server->worker_pid, 'Task#' . $server->worker_id);
 
 		Snowflake::setTaskId($server->worker_pid);
@@ -90,9 +85,8 @@ class OnWorkerStart extends Callback
 			name($server->worker_pid, 'Worker#' . $server->worker_id);
 
 			$time = microtime(true);
-			$annotation->runtime(CONTROLLER_PATH);
+			$annotation->runtime(APP_PATH);
 			$this->debug('use time.' . microtime(true) - $time);
-			$annotation->runtime(directory('app'), CONTROLLER_PATH);
 
 			Snowflake::setWorkerId($server->worker_pid);
 			putenv('environmental=' . Snowflake::WORKER);
