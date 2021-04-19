07718481143e644feeff138fdfe9b055baafcdfc
Mr·x <as2252258@163.com>
2021-04-19 15:31:48 +0800

    改名

--- Annotation/Route/Socket.php
+++ Annotation/Route/Socket.php
@@ -50,7 +50,6 @@ use Snowflake\Snowflake;
 
 		$method = $this->event . '::' . (is_null($this->uri) ? 'event' : $this->uri);
 
-		var_dump($method);
 		$router->addRoute($method, $handler, 'sw::socket');
 
 		return $router;
