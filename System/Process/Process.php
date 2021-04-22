f9210d997112899f559773ff267ec878273ae8b8
as2252258@163.com <as22522582>
2021-04-23 02:33:23 +0800

    modify

--- System/Process/Process.php
+++ System/Process/Process.php
@@ -20,10 +20,7 @@ use Swoole\Coroutine\System;
 abstract class Process extends \Swoole\Process implements SProcess
 {
 
-    /** @var Application $application */
-    protected Application $application;
-
-
+    
     /**
      * Process constructor.
      * @param $application
