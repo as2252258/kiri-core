96266e3f21142513e56dc86fd8fb2c8d0518f994
as2252258@163.com <as22522582>
2021-04-24 19:34:39 +0800

    modify

--- Annotation/Loader.php
+++ Annotation/Loader.php
@@ -425,13 +425,12 @@ class Loader extends BaseObject
      */
     private function _execute($class, $annotation, $handler, $value, $name)
     {
-        var_dump($handler);
         if ($value instanceof Relation) {
-            $annotation->addGets($handler, $value->name, $name);
-        } else if ($value instanceof Get) {
-            $annotation->addSets($handler, $value->name, $name);
-        } else if ($value instanceof Set) {
             $annotation->addRelate($handler, $value->name, $name);
+        } else if ($value instanceof Get) {
+            $annotation->addGets($handler, $value->name, $name);
+        } else if ($value instanceof Set) {
+            $annotation->addSets($handler, $value->name, $name);
         } else {
             $value->execute([$class, $name]);
         }
