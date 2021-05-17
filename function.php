fd5fef2af0b7230ab95740dc16fd985e9dee43c8
Mr·x <as2252258@163.com>
2021-05-17 17:33:42 +0800

    改名

--- function.php
+++ function.php
@@ -678,6 +678,11 @@ if (!function_exists('sweep')) {
 	{
 		$array = [];
 		foreach (glob($configPath . '/*') as $config) {
+
+			var_dump($config);
+
+			var_dump(require_once "$config");
+
 			$array = array_merge(require_once "$config", $array);
 		}
 		return $array;
