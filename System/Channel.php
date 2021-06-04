b6ac2c75ee96e17405438723516d06fe6192aae8
Mr·x <as2252258@163.com>
2021-06-04 18:49:31 +0800

    改名

--- System/Channel.php
+++ System/Channel.php
@@ -18,54 +18,83 @@ class Channel extends Component
 {
 
 
-    private static array $_channels = [];
+	private static array $_channels = [];
 
 
-    /**
-     * @param mixed $value
-     * @param string $name
-     * @throws Exception
-     */
-    public function push(mixed $value, string $name = ''): void
-    {
-        $channel = $this->channelInit($name);
-        if ($channel->count() >= 100) {
-            return;
-        }
-        $channel->enqueue($value);
-    }
+	private static array $_waitRecover = [];
 
 
-    /**
-     * @param string $name
-     * @return bool|SplQueue
-     */
-    private function channelInit(string $name = ''): bool|SplQueue
-    {
-        if (!isset(static::$_channels[$name]) || !(static::$_channels[$name] instanceof SplQueue)) {
-            static::$_channels[$name] = new SplQueue();
-        }
-        return static::$_channels[$name];
-    }
+	public function init()
+	{
+		Event::on(Event::SYSTEM_RESOURCE_RELEASES, [$this, 'recover']);
+	}
 
 
-    /**
-     *
-     * 清空缓存
-     */
-    public function cleanAll()
-    {
-        /** @var SplQueue $channel */
-        foreach (static::$_channels as $channel) {
-            if (!($channel instanceof SplQueue)) {
-                continue;
-            }
-            while ($channel->count() > 0) {
-                $channel->dequeue();
-            }
-        }
-        static::$_channels = [];
-    }
+	/**
+	 * 回收对象
+	 */
+	public function recover()
+	{
+		foreach (Channel::$_waitRecover as $key => $value) {
+			if (empty($value)) {
+				continue;
+			}
+			$channel = $this->channelInit($key);
+			if ($channel->count() >= 100) {
+				continue;
+			}
+			foreach ($value as $item) {
+				$channel->enqueue($item);
+			}
+		}
+		Channel::$_waitRecover = [];
+	}
+
+
+	/**
+	 * @param mixed $value
+	 * @param string $name
+	 * @throws Exception
+	 */
+	public function push(mixed $value, string $name = ''): void
+	{
+		if (!isset(Channel::$_waitRecover[$name])) {
+			Channel::$_waitRecover[$name] = [];
+		}
+		Channel::$_waitRecover[$name][] = $value;
+	}
+
+
+	/**
+	 * @param string $name
+	 * @return bool|SplQueue
+	 */
+	private function channelInit(string $name = ''): bool|SplQueue
+	{
+		if (!isset(static::$_channels[$name]) || !(static::$_channels[$name] instanceof SplQueue)) {
+			static::$_channels[$name] = new SplQueue();
+		}
+		return static::$_channels[$name];
+	}
+
+
+	/**
+	 *
+	 * 清空缓存
+	 */
+	public function cleanAll()
+	{
+		/** @var SplQueue $channel */
+		foreach (static::$_channels as $channel) {
+			if (!($channel instanceof SplQueue)) {
+				continue;
+			}
+			while ($channel->count() > 0) {
+				$channel->dequeue();
+			}
+		}
+		static::$_channels = [];
+	}
 
 
 	/**
@@ -73,14 +102,14 @@ class Channel extends Component
 	 * @param Closure $closure
 	 * @return mixed
 	 */
-    public function pop(string $name, Closure $closure): mixed
-    {
-        $channel = $this->channelInit($name);
-        if ($channel->isEmpty()) {
-            return call_user_func($closure);
-        }
-        return clone $channel->dequeue();
-    }
+	public function pop(string $name, Closure $closure): mixed
+	{
+		$channel = $this->channelInit($name);
+		if ($channel->isEmpty()) {
+			return call_user_func($closure);
+		}
+		return $channel->dequeue();
+	}
 
 
 }
