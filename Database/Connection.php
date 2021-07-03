2e32dea501d41d60a0d4c416f91955f732b8e9df
as2252258@163.com <as22522582>
2021-07-03 13:39:32 +0800

    modify

--- Database/Connection.php
+++ Database/Connection.php
@@ -265,7 +265,7 @@ class Connection extends Component
      */
     public function createCommand($sql = null, array $attributes = []): Command
     {
-        $substr = strtoupper(substr($sql, 1, 6));
+        $substr = strtoupper(substr($sql, 0, 5));
 
         var_dump($substr);
 
