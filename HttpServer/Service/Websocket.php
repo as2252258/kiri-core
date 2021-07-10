bd1a34985b287ae49cd410677b12a17033e160b9
as2252258@163.com <as22522582>
2021-07-11 04:48:14 +0800

    modify

--- HttpServer/Service/Websocket.php
+++ HttpServer/Service/Websocket.php
@@ -8,29 +8,37 @@ use ReflectionException;
 use Snowflake\Exception\NotFindClassException;
 use HttpServer\Service\Abstracts\Websocket as HAWebsocket;
 
+
+/**
+ * Class Websocket
+ * @package HttpServer\Service
+ */
 class Websocket extends HAWebsocket
 {
 
-	/**
-	 * @throws ReflectionException
-	 * @throws NotFindClassException
-	 */
-	public function onInit()
-	{
-		$this->onHandlerListener();
-		$this->onBaseListener();
-	}
+    /**
+     * @throws ReflectionException
+     * @throws NotFindClassException
+     */
+    public function onInit()
+    {
+        $this->onHandlerListener();
+        $this->onBaseListener();
+    }
 
 
-	/**
-	 * @throws NotFindClassException
-	 * @throws ReflectionException
-	 */
-	public function onBaseListener()
-	{
-		$this->on('handshake', $this->createHandler('handshake'));
-		$this->on('message', $this->createHandler('message'));
-		$this->on('close', $this->createHandler('close'));
-	}
+    /**
+     * @throws NotFindClassException
+     * @throws ReflectionException
+     */
+    public function onBaseListener()
+    {
+        $this->on('connect', function () {
+
+        });
+        $this->on('handshake', $this->createHandler('handshake'));
+        $this->on('message', $this->createHandler('message'));
+        $this->on('close', $this->createHandler('close'));
+    }
 
 }
