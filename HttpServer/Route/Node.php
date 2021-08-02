5b473b2d5ada1d63e908be7b29e7fd9fea3a96c8
Mr·x <as2252258@163.com>
2021-08-02 18:37:22 +0800

    改名

--- HttpServer/Route/Node.php
+++ HttpServer/Route/Node.php
@@ -164,7 +164,7 @@ class Node extends HttpService
 	{
 		/** @var Aop $aop */
 		$aop = Snowflake::app()->get('aop');
-		var_dump($this->path . '->' . $aop->hasAop($this->handler));
+		var_dump($this->path . '->' . (int)$aop->hasAop($this->handler));
 		if ($this->handler instanceof Closure || !$aop->hasAop($this->handler)) {
 			return $this->normalHandler($this->handler);
 		} else {
