633c20fdcd6f31a8f1667e2c56096a2e94d8925d
Mr·x <as2252258@163.com>
2021-08-03 18:27:28 +0800

    改名

--- System/Events/EventProvider.php
+++ System/Events/EventProvider.php
@@ -22,9 +22,6 @@ class EventProvider implements EventProviders
 		$queue = new \SplPriorityQueue();
 		// TODO: Implement getListenersForEvent() method.
 		foreach ($this->_listeners[get_class($event)] ?? [] as $listener) {
-			if (!($event instanceof $listener->event)) {
-				continue;
-			}
 			$queue->insert($listener, $listener->priority);
 		}
 		return $queue;
