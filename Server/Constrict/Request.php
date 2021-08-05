b13d1d44bd4d9f9f3de67bd8933c573d201f20e3
as2252258@163.com <as22522582>
2021-08-05 22:55:23 +0800

    modify

--- Server/Constrict/Request.php
+++ Server/Constrict/Request.php
@@ -45,25 +45,21 @@ class Request implements RequestInterface
 	public static function create(\Swoole\Http\Request $request): RequestInterface
 	{
         Context::setContext(Response::class, new Response());
-        try {
-            $sRequest = new HttpResponse();
 
-            $sRequest->headers = new HttpHeaders();
-            $sRequest->headers->setHeaders(array_merge($request->header, $request->server));
+        $sRequest = new HttpResponse();
 
-            var_dump($sRequest->headers->toArray());
+        $sRequest->headers = new HttpHeaders();
+        $sRequest->headers->setHeaders(array_merge($request->header, $request->server));
 
-            $sRequest->setUri($sRequest->headers->getRequestUri());
-            $sRequest->setClientId($request->fd);
+        $sRequest->setUri($sRequest->headers->getRequestUri());
+        $sRequest->setClientId($request->fd);
 
-            $sRequest->params = new HttpParams();
-            $sRequest->params->setRawContent($request->rawContent(), $sRequest->headers->getContentType());
-            $sRequest->params->setFiles($request->files);
-            $sRequest->params->setPosts($request->post);
-            $sRequest->params->setGets($request->get);
-        }catch (\Throwable $exception){
-            var_dump($exception);
-        }
+        $sRequest->params = new HttpParams();
+        $sRequest->params->setRawContent($request->rawContent(), $sRequest->headers->getContentType());
+        $sRequest->params->setFiles($request->files);
+        $sRequest->params->setPosts($request->post);
+        $sRequest->params->setGets($request->get);
+
         Context::setContext(HttpResponse::class, $sRequest);
 
 
