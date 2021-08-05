35cf073f0b5b8e0df3cb83625ac6b90bd383b002
Mr·x <as2252258@163.com>
2021-08-05 16:21:31 +0800

    改名

--- Server/Manager/OnServerManager.php
+++ Server/Manager/OnServerManager.php
@@ -4,6 +4,7 @@ namespace Server\Manager;
 
 use Server\Abstracts\Server;
 use Server\Constant;
+use Snowflake\Exception\ConfigException;
 
 
 /**
@@ -16,7 +17,7 @@ class OnServerManager extends Server
 
     /**
      * @param \Swoole\Server $server
-     * @throws \Snowflake\Exception\ConfigException
+     * @throws ConfigException
      */
 	public function onManagerStart(\Swoole\Server $server)
 	{
