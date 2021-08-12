07312d25fe460fd92e6687c7c6827fe2f7c6ba3f
Mr·x <as2252258@163.com>
2021-08-12 15:09:25 +0800

    改名

--- Server/Constrict/ResponseEmitter.php
+++ Server/Constrict/ResponseEmitter.php
@@ -14,7 +14,7 @@ use Swoole\Server;
 /**
  *
  */
-class ResponseEmitter
+class ResponseEmitter implements Emitter
 {
 
 
@@ -25,7 +25,7 @@ class ResponseEmitter
 	 * @throws ReflectionException
 	 * @throws Exception
 	 */
-	public function sender(\Swoole\Http\Response|\Swoole\Http2\Response $response, ResponseInterface $emitter): void
+	public function sender(mixed $response, ResponseInterface $emitter): void
 	{
 		$content = $emitter->configure($response)->getContent();
 		if ($content instanceof FileFormatter) {
