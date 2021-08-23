c1c0d862b9ccfa5d1eff0837a1c7009d987ec615
Mr·x <as2252258@163.com>
2021-08-23 10:53:20 +0800

    改名

--- Kafka/Kafka.php
+++ Kafka/Kafka.php
@@ -127,7 +127,6 @@ class Kafka extends CustomProcess
 				/** @var KafkaProvider $container */
 				$container = Kiri::getDi()->get(KafkaProvider::class);
 				$data = $container->getConsumer($topic);
-				var_dump($data, $container);
 				if (!empty($data)) {
 					$server->sendMessage(new $data(new Struct($topic, $message)), random_int(0, $setting - 1));
 				}
