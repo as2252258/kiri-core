3b2654013b87be5c02ccc37f182ad8bfa4a190c0
as2252258@163.com <as22522582>
2021-09-23 01:06:59 +0800

    111

--- http-server/Service/Http.php
+++ http-server/Service/Http.php
@@ -74,7 +74,7 @@ class Http extends \Server\Abstracts\Http implements OnClose, OnConnect
             include_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_lib.php";
             include_once $XHPROF_ROOT . "/xhprof_lib/utils/xhprof_runs.php";
 
-            $xhprof_runs = new \XHProfRuns_Default();
+            $xhprof_runs = new \XHProfRuns_Default(storage('xhprof'));
             $run_id = $xhprof_runs->save_run($xhprof_data, "xhprof_testing");
 
             echo "http://localhost/xhprof/xhprof_html/index.php?run={$run_id}&source=xhprof_testing\n";
