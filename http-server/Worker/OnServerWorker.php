9e50cad232d907ab1a6f46c90431890e59f51d47
Mr·x <as2252258@163.com>
2021-09-22 11:48:45 +0800

    改名

--- http-server/Worker/OnServerWorker.php
+++ http-server/Worker/OnServerWorker.php
@@ -10,6 +10,7 @@ use Kiri\Events\EventDispatch;
 use Kiri\Runtime;
 use Server\Events\OnAfterWorkerStart;
 use Server\Events\OnBeforeWorkerStart;
+use Server\Events\OnTaskerStart as OnTaskStart;
 use Server\Events\OnWorkerError;
 use Server\Events\OnWorkerExit;
 use Server\Events\OnWorkerStart;
@@ -17,7 +18,6 @@ use Server\Events\OnWorkerStop;
 use Server\ServerManager;
 use Swoole\Server;
 use Swoole\Timer;
-use Server\Events\OnTaskerStart as OnTaskStart;
 
 
 /**
@@ -28,108 +28,108 @@ class OnServerWorker extends \Server\Abstracts\Server
 {
 
 
-    /**
-     * @var EventDispatch
-     */
-    #[Inject(EventDispatch::class)]
-    public EventDispatch $eventDispatch;
+	/**
+	 * @var EventDispatch
+	 */
+	#[Inject(EventDispatch::class)]
+	public EventDispatch $eventDispatch;
 
 
-    /**
-     * @param Server $server
-     * @param int $workerId
-     * @throws Exception
-     */
-    public function onWorkerStart(Server $server, int $workerId)
-    {
-        $this->eventDispatch->dispatch(new OnBeforeWorkerStart($workerId));
-        if ($workerId < $server->setting['worker_num']) {
-            $this->eventDispatch->dispatch(new OnWorkerStart($server, $workerId));
-            $this->setProcessName(sprintf('Worker[%d].%d', $server->worker_pid, $workerId));
-        } else {
-            $this->eventDispatch->dispatch(new OnTaskStart($server, $workerId));
-            $this->setProcessName(sprintf('Tasker[%d].%d', $server->worker_pid, $workerId));
-        }
-        $this->eventDispatch->dispatch(new OnAfterWorkerStart());
-    }
+	/**
+	 * @param Server $server
+	 * @param int $workerId
+	 * @throws Exception
+	 */
+	public function onWorkerStart(Server $server, int $workerId)
+	{
+		$this->eventDispatch->dispatch(new OnBeforeWorkerStart($workerId));
+		if ($workerId < $server->setting['worker_num']) {
+			$this->eventDispatch->dispatch(new OnWorkerStart($server, $workerId));
+			$this->setProcessName(sprintf('Worker[%d].%d', $server->worker_pid, $workerId));
+		} else {
+			$this->eventDispatch->dispatch(new OnTaskStart($server, $workerId));
+			$this->setProcessName(sprintf('Tasker[%d].%d', $server->worker_pid, $workerId));
+		}
+		$this->eventDispatch->dispatch(new OnAfterWorkerStart());
+	}
 
 
 	/**
 	 * @param OnBeforeWorkerStart $worker
 	 * @throws Exception
 	 */
-    public function setConfigure(OnBeforeWorkerStart $worker)
-    {
-        ServerManager::setEnv('worker', $worker->workerId);
-        $serialize = file_get_contents(storage(Runtime::CONFIG_NAME));
-        if (!empty($serialize)) {
-            Config::sets(unserialize($serialize));
-        }
-    }
+	public function setConfigure(OnBeforeWorkerStart $worker)
+	{
+		ServerManager::setEnv('worker', $worker->workerId);
+		$serialize = file_get_contents(storage(Runtime::CONFIG_NAME));
+		if (!empty($serialize)) {
+			Config::sets(unserialize($serialize));
+		}
+	}
 
 
-    /**
-     * @param Server $server
-     * @param int $workerId
-     * @throws Exception
-     */
-    public function onWorkerStop(Server $server, int $workerId)
-    {
-        $this->eventDispatch->dispatch(new OnWorkerStop($server, $workerId));
+	/**
+	 * @param Server $server
+	 * @param int $workerId
+	 * @throws Exception
+	 */
+	public function onWorkerStop(Server $server, int $workerId)
+	{
+		$this->eventDispatch->dispatch(new OnWorkerStop($server, $workerId));
 
-        Timer::clearAll();
-    }
-
-
-    /**
-     * @param Server $server
-     * @param int $workerId
-     * @throws Exception
-     */
-    public function onWorkerExit(Server $server, int $workerId)
-    {
-        $this->eventDispatch->dispatch(new OnWorkerExit($server, $workerId));
-
-        ServerManager::setEnv('state', 'exit');
-    }
+		Timer::clearAll();
+	}
 
 
-    /**
-     * @param Server $server
-     * @param int $worker_id
-     * @param int $worker_pid
-     * @param int $exit_code
-     * @param int $signal
-     * @throws Exception
-     */
-    public function onWorkerError(Server $server, int $worker_id, int $worker_pid, int $exit_code, int $signal)
-    {
-        $this->eventDispatch->dispatch(new OnWorkerError($server, $worker_id, $worker_pid, $exit_code, $signal));
+	/**
+	 * @param Server $server
+	 * @param int $workerId
+	 * @throws Exception
+	 */
+	public function onWorkerExit(Server $server, int $workerId)
+	{
+		$this->eventDispatch->dispatch(new OnWorkerExit($server, $workerId));
 
-        $message = sprintf('Worker#%d::%d error stop. signal %d, exit_code %d, msg %s',
-            $worker_id, $worker_pid, $signal, $exit_code, swoole_strerror(swoole_last_error(), 9)
-        );
-
-        $this->logger->error($message);
-
-        $this->system_mail($message);
-    }
+		ServerManager::setEnv('state', 'exit');
+	}
 
 
-    /**
-     * @param $messageContent
-     * @throws Exception
-     */
-    protected function system_mail($messageContent)
-    {
-        try {
-            $email = Config::get('email');
-            if (!empty($email) && ($email['enable'] ?? false) == true) {
-                Help::sendEmail($email, 'Service Error', $messageContent);
-            }
-        } catch (\Throwable $e) {
-            error($e, 'email');
-        }
-    }
+	/**
+	 * @param Server $server
+	 * @param int $worker_id
+	 * @param int $worker_pid
+	 * @param int $exit_code
+	 * @param int $signal
+	 * @throws Exception
+	 */
+	public function onWorkerError(Server $server, int $worker_id, int $worker_pid, int $exit_code, int $signal)
+	{
+		$this->eventDispatch->dispatch(new OnWorkerError($server, $worker_id, $worker_pid, $exit_code, $signal));
+
+		$message = sprintf('Worker#%d::%d error stop. signal %d, exit_code %d, msg %s',
+			$worker_id, $worker_pid, $signal, $exit_code, swoole_strerror(swoole_last_error(), 9)
+		);
+
+		$this->logger->error($message);
+
+		$this->system_mail($message);
+	}
+
+
+	/**
+	 * @param $messageContent
+	 * @throws Exception
+	 */
+	protected function system_mail($messageContent)
+	{
+		try {
+			$email = Config::get('email');
+			if (!empty($email) && ($email['enable'] ?? false) == true) {
+				Help::sendEmail($email, 'Service Error', $messageContent);
+			}
+		} catch (\Throwable $e) {
+			error($e, 'email');
+		}
+	}
 
 }
