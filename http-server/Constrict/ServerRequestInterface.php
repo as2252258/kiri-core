83daee274b09bbdbf678ab9a5243c2138385aee5
向林 <as2252258@163.com>
2021-10-19 15:55:45 +0800

    改名

---
+++ http-server/Constrict/ServerRequestInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Server\Constrict;
+
+
+use Http\Message\ServerRequest;
+
+/**
+ * @mixin ServerRequest
+ */
+interface ServerRequestInterface extends \Psr\Http\Message\ServerRequestInterface
+{
+
+}
