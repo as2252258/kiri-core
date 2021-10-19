dacf0d2b116bc05c3fc91ce73762467581b7c995
向林 <as2252258@163.com>
2021-10-19 16:40:17 +0800

    改名

--- http-server/ServerCommand.php
+++ http-server/ServerCommand.php
@@ -52,7 +52,7 @@ class ServerCommand extends Command
 		$this->setName('sw:server')
 			->setDescription('server start|stop|reload|restart')
 			->addArgument('action', InputArgument::REQUIRED)
-			->addOption('--daemon', '-d');
+			->addOption('--daemon');
 	}
 
 
@@ -66,6 +66,7 @@ class ServerCommand extends Command
 	{
 		try {
 			$manager = Kiri::app()->getServer();
+			var_dump((int)$input->hasOption('--daemon'));
 			$manager->setDaemon((int)$input->hasOption('--daemon'));
 			if (!in_array($input->getArgument('action'), self::ACTIONS)) {
 				throw new Exception('I don\'t know what I want to do.');
