79b32df2d5512482f36a78099b46bbd773ecc817
向林 <as2252258@163.com>
2021-11-04 19:00:53 +0800

    改名

--- kiri-engine/FileListen/FileChangeCustomProcess.php
+++ kiri-engine/FileListen/FileChangeCustomProcess.php
@@ -104,10 +104,12 @@ class FileChangeCustomProcess extends Command
 	{
 		Kiri::getDi()->get(Logger::class)->warning('change reload');
 
-		$content = file_get_contents(storage('.swoole.pid'));
+		$content = (int)file_get_contents(storage('.swoole.pid'));
 		if (!empty($content)) {
-			var_dump($content);
-			Process::kill((int)$content, SIGTERM);
+			var_dump($content, Process::kill($content, 0));
+			if (Process::kill($content, 0)) {
+				Process::kill($content, SIGTERM);
+			}
 		}
 		proc_open("php " . APP_PATH . "kiri.php", [], $pipes);
 	}
