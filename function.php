6168adb401347291b2dfdf050630bbef0bbef9df
向林 <as2252258@163.com>
2022-01-12 18:12:56 +0800

    Revert "改名"
    This reverts commit fdf58326

--- function.php
+++ function.php
@@ -1252,7 +1252,7 @@ Throwable: ' . $throwable->getMessage() . "\t\n" . '	' . $throwable->getFile() .
 			$message .= "\t" . '
 		' . $value['file'] . '->' . $value['line'] . '(' . ($value['class'] ?? 'static') . '::' . ($value['function'] ?? 'function') . ')' . "\t";
 		}
-		return print_r("\033[1;31,37m\t\t" . $message . "\t\t\33[41m", true);
+		return "\033[41;37m[" . $message . "\033[0m";
 	}
 
 }
