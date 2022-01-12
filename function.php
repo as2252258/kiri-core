2daac841a3574944020ecc4cd776315ff3d8cf1a
向林 <as2252258@163.com>
2022-01-12 18:49:57 +0800

    Revert "改名"
    This reverts commit fdf58326

--- function.php
+++ function.php
@@ -1248,7 +1248,7 @@ if (!function_exists('error_trigger_format')) {
 			if (!isset($value['file'])) {
 				continue;
 			}
-			$message .= "\n\t" . $value['file'] . " -> " . $value['line'] . "(" . (isset($value['class']) ? $value['class'] . '::' : '') . ($value['function'] ?? 'Closure') . ")";
+			$message .= "\n\t\t\t\t\t" . $value['file'] . " -> " . $value['line'] . "(" . (isset($value['class']) ? $value['class'] . '::' : '') . ($value['function'] ?? 'Closure') . ")";
 		}
 		return "\033[41;37m" . $message . "\033[0m";
 	}
