d2dd255c90ae60a82d77a567900d83782842ef2b
向林 <as2252258@163.com>
2022-06-24 15:07:46 +0800

    modify plugin name

--- kiri-annotation/AbstractAttribute.php
+++ kiri-annotation/AbstractAttribute.php
@@ -4,6 +4,8 @@
 namespace Kiri\Annotation;
 
 
+use ReturnTypeWillChange;
+
 /**
  * Class Attribute
  * @package Annotation
@@ -12,16 +14,55 @@ abstract class AbstractAttribute implements IAnnotation
 {
 
 
-    /**
-     * @param static $class
-     * @param mixed|string $method
-     * @return mixed
-     */
-    #[\Kiri\ReturnTypeWillChange]
-    public function execute(mixed $class, mixed $method = ''): mixed
-    {
-        // TODO: Implement execute() method.
-        return true;
-    }
+	protected object $class;
+
+
+	protected string $method;
+
+
+	/**
+	 * @param static $class
+	 * @param mixed|string $method
+	 * @return mixed
+	 */
+	#[ReturnTypeWillChange]
+	public function execute(mixed $class, mixed $method = ''): mixed
+	{
+		// TODO: Implement execute() method.
+		return true;
+	}
+
+	/**
+	 * @return object
+	 */
+	public function getClass(): object
+	{
+		return $this->class;
+	}
+
+	/**
+	 * @param object $class
+	 */
+	public function setClass(object $class): void
+	{
+		$this->class = $class;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getMethod(): string
+	{
+		return $this->method;
+	}
+
+	/**
+	 * @param string $method
+	 */
+	public function setMethod(string $method): void
+	{
+		$this->method = $method;
+	}
+
 
 }
