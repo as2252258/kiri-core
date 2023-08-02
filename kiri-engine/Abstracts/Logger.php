1928ddd350339ab140af7610c51ebb5f24565a46
向林 <as2252258@163.com>
2023-08-02 12:48:39 +0800

    qqq

--- kiri-engine/Abstracts/Logger.php
+++ kiri-engine/Abstracts/Logger.php
@@ -233,7 +233,6 @@ class Logger implements LoggerInterface
      */
     private function _string($context): string
     {
-        var_dump($context);
         if (is_string($context)) {
             return $context . PHP_EOL;
         }
