483dd6bc79c6a34c3ebf09f067c478ce0fef5554
向林 <as2252258@163.com>
2023-08-17 15:52:02 +0800

    qqq

--- function.php
+++ function.php
@@ -870,7 +870,7 @@ if (!function_exists('error')) {
      */
     function error(mixed $message, array $method = []): void
     {
-        if (is_string($message) && str_contains($message, 'inotify_rm_watch(): The file descriptor is not an inotify instance or the watch descriptor')) {
+        if (is_string($message) && str_contains($message, 'inotify_rm_watch')) {
             return;
         }
         Kiri::getLogger()->failure($message);
