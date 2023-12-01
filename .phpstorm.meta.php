9a5384e3ef6a93feb2081e961c17ef1e7fd5fc9f
whwyy <as2252258@163.com>
2023-12-01 22:57:25 +0800

    eee

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -6,8 +6,9 @@ namespace PHPSTORM_META {
 	use Kiri\Di\Container;
 	use Psr\Container\ContainerInterface;
 	use Psr\Container\ContainerInterface as SC;
+    use Psr\Http\Message\ServerRequestInterface;
 
-	override(ContainerInterface::get(0), map('@'));
+    override(ContainerInterface::get(0), map('@'));
 	override(SC::get(0), map('@'));
 	override(Container::get(0), map('@'));
 	override(Container::make(0), map('@'));
@@ -16,5 +17,6 @@ namespace PHPSTORM_META {
     override(\make(0), map('@'));
     override(\di(0), map('@'));
     override(\duplicate(0), map('@'));
+    override(ServerRequestInterface::getAttribute(0), map('@'));
 
 }
