827fb257abcabc26869ec8f47b32c8f729d973d3
whwyy <as2252258@163.com>
2023-12-12 14:10:09 +0800

    eee

--- p.php
+++ p.php
@@ -1,14 +1,1 @@
 <?php
-
-use function Swoole\Coroutine\run;
-
-run(function () {
-
-    $channel = new \Swoole\Coroutine\Channel(100);
-    for ($i = 0; $i < 90; $i++) {
-        $channel->push(100);
-    }
-    $channel->close();
-    $channel = null;
-    var_dump($channel);
-});
